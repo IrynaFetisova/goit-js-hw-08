85b4dd81ab30076a0125d4c80012606f744668b3
IrynaFetisova <109384375+IrynaFetisova@users.noreply.github.com>
2022-09-28 20:53:20 +0300

    hw-js8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,28 @@
 import throttle from "lodash.throttle";
 
-const formEl = document.querySelector(".feedback-form");
-const emailEl = document.querySelector(".feedback-form input");
-const textareaEl = document.querySelector(".feedback-form textarea");
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector(".feedback-form input");
+const textarea = document.querySelector(".feedback-form textarea");
 
 
 const FORM_STATE = "feedback-form-state";
 let formData = {};
 formFillOut()
 
-formEl.addEventListener('input', throttle(onInputChange, 500));
-formEl.addEventListener('submit',  onFormSubmit)
+form.addEventListener('input', throttle(onInputChange, 500));
+form.addEventListener('submit',  onFormSubmit)
 
 function onInputChange(evt){
 
     formData[evt.target.name] = evt.target.value; //  formData.email = emailEl.value; formData.message = textareaEl.value;
     localStorage.setItem(FORM_STATE, JSON.stringify(formData));
-    // console.log(formData);
+   
 
 }
 function onFormSubmit (evt) {
     evt.preventDefault();
     console.log(JSON.parse(localStorage.getItem(FORM_STATE)))
-    // console.log('Message sent!');
+  
     evt.target.reset();
     formData = {};
     localStorage.removeItem(FORM_STATE);
@@ -36,11 +36,7 @@ if (savedData === null || savedData === undefined) {
 formData = savedData;
 
 if(savedData){
-    // console.log(savedData);
-    // console.log(emailEl.value);
-    // console.log(textareaEl.value);
-
-    emailEl.value = savedData.email ? savedData.email : emailEl.value;
-    textareaEl.value = savedData.message ? savedData.message : textareaEl.value;
+    email.value = savedData.email ? savedData.email : email.value;
+    textarea.value = savedData.message ? savedData.message : textarea.value;
 }
 };
